23fc1761f89e83681d7dd0ca748699af387fe4aa
Anastasia Diseth <annadiseth@gmail.com>
2020-11-11 01:05:01 +0200

    pom configuroitu kunnolla, toimii paikallisesti

--- SnakeGame/src/main/java/anadis/snakegame/ui/Ui.java
+++ SnakeGame/src/main/java/anadis/snakegame/ui/Ui.java
@@ -6,11 +6,17 @@
 package anadis.snakegame.ui;
 
 import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
  *
  * @author anadis
  */
 public class Ui extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }

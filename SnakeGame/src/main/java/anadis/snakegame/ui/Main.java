c6417138305f660c0862090bbd58ebce6d21de28
Anastasia Diseth <annadiseth@gmail.com>
2020-11-11 00:00:13 +0200

    Checking javafx configuration

--- SnakeGame/src/main/java/anadis/snakegame/ui/Main.java
+++ SnakeGame/src/main/java/anadis/snakegame/ui/Main.java
@@ -1,9 +1,11 @@
+package anadis.snakegame.ui;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package anadis.snakegame.ui;
+
 
 /**
  *
---
+++ SnakeGame/src/main/java/anadis/snakegame/ui/Ui.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anadis.snakegame.ui;
+
+import javafx.application.Application;
+
+/**
+ *
+ * @author anadis
+ */
+public class Ui extends Application {
+    
+}

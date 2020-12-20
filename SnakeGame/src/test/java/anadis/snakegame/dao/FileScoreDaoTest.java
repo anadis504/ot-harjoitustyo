627af80b823f96a3e87479b23476a8bb77874202
Anastasia Diseth <annadiseth@gmail.com>
2020-12-20 17:18:52 +0200

    refactor daoTest to follow TemporaryFolder rule

--- SnakeGame/src/test/java/anadis/snakegame/dao/FileScoreDaoTest.java
+++ SnakeGame/src/test/java/anadis/snakegame/dao/FileScoreDaoTest.java
@@ -11,6 +11,10 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.io.File;
+import java.io.FileWriter;
+import java.time.LocalDateTime;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
 
 /**
  *
@@ -18,34 +22,51 @@ import java.io.File;
  */
 public class FileScoreDaoTest {
 
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
     FileScoreDao scoreDao;
-    File test = new File("testfilescoreDao.txt");
+    File testFile;
 
     public FileScoreDaoTest() {
     }
 
     @Before
     public void setUp() throws Exception {
-        this.scoreDao = new FileScoreDao("testfilescoreDao.txt");
-    }
+        this.testFile = testFolder.newFile("testfile_scores.txt");
+        LocalDateTime timestamp = java.time.LocalDateTime.now();
 
-    @After
-    public void tearDown() {
-        test.deleteOnExit();
+        try (FileWriter writer = new FileWriter(testFile.getAbsoluteFile())) {
+            writer.write("bob;12;1;" + timestamp);
+        }
+
+        this.scoreDao = new FileScoreDao(testFile.getAbsolutePath());
     }
 
     @Test
-    public void testfileExistsAndNotEmptyAfterFirstAddition() {
-        scoreDao.add(new Score("bob", 12, 1, null));
+    public void testfileExistsAndNotEmpty() {
         assertFalse(scoreDao.getAll().isEmpty());
     }
 
     @Test
     public void highestScoreComesFirst() {
-        scoreDao.add(new Score("bob", 12, 1, null));
-        scoreDao.add(new Score("alice", 13, 1, null));
+        scoreDao.add(new Score("alice", 13, 1, java.time.LocalDateTime.now()));
         assertEquals("alice", scoreDao.getAll().get(0).getName());
         assertEquals(13, scoreDao.getAll().get(0).getScore());
     }
-    
+
+    @Test
+    public void lowestLevelComesFirst() {
+        scoreDao.add(new Score("bob", 22, 2, java.time.LocalDateTime.now()));
+        assertEquals(1, scoreDao.getAll().get(0).getLevel());
+        assertEquals(22, scoreDao.getAll().get(1).getScore());
+    }
+
+    @Test
+    public void scoresAreSortedByTimestamp() {
+        scoreDao.add(new Score("alice", 12, 1, java.time.LocalDateTime.now()));
+        assertEquals("alice", scoreDao.getAll().get(1).getName());
+        assertEquals("bob", scoreDao.getAll().get(0).getName());
+    }
+
 }
